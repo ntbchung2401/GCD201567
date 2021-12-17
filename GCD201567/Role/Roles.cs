6b8fb476e0a8b973d2a1574ca5b2bb0ab028d8a1
Chung Nguyễn <nguyenthachbaochung@gmail.com>
2021-12-18 00:52:16 +0700

    RegisterNewUserAsRoleStudent

---
+++ GCD201567/Role/Roles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GCD201567.Role
+{
+    public class Roles
+    {
+        public const string Student = "student";
+        public const string Teacher = "teacher";
+        public const string Staff = "staff";
+    }
+}
